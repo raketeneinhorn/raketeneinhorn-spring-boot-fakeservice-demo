6dfcdc8cacad96e23acd062644d9e9b22e131a92
Raketeneinhorn GmbH <account+github@raketeneinhorn.com>
2024-08-07 20:01:01 +0200

    wip: test if that changes coverage information on sonarcloud

---
+++ fakeservice-demo-base/src/main/java/com/raketeneinhorn/spring/boot/fakeservice/demo/core/unicorn/RemoveMeAfterFixingCoverageReport.java
@@ -0,0 +1,9 @@
+package com.raketeneinhorn.spring.boot.fakeservice.demo.core.unicorn;
+
+public class RemoveMeAfterFixingCoverageReport {
+
+    public String foo(String bar) {
+        return bar + "baz";
+    }
+
+}
