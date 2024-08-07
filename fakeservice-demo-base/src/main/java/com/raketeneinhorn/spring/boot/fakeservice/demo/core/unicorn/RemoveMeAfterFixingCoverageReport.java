2e7b542ec262a009120a63672382011562db1f5d
Raketeneinhorn GmbH <account+github@raketeneinhorn.com>
2024-08-07 20:36:15 +0200

    wip: remove debugging code

--- fakeservice-demo-base/src/main/java/com/raketeneinhorn/spring/boot/fakeservice/demo/core/unicorn/RemoveMeAfterFixingCoverageReport.java
+++
@@ -1,9 +0,0 @@
-package com.raketeneinhorn.spring.boot.fakeservice.demo.core.unicorn;
-
-public class RemoveMeAfterFixingCoverageReport {
-
-    public String foo(String bar) {
-        return bar + "baz";
-    }
-
-}
