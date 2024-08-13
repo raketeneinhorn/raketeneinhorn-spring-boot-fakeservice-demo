0d311c1cfb4d1c8f1f304d00801140d8f8231896
Raketeneinhorn GmbH <account+github@raketeneinhorn.com>
2024-08-13 20:18:04 +0200

    feat: add logging statements

--- fakeservice-demo-base/src/main/java/com/raketeneinhorn/spring/boot/fakeservice/demo/core/unicorn/NiceUnicornService.java
+++ fakeservice-demo-base/src/main/java/com/raketeneinhorn/spring/boot/fakeservice/demo/core/unicorn/NiceUnicornService.java
@@ -1,14 +1,18 @@
 package com.raketeneinhorn.spring.boot.fakeservice.demo.core.unicorn;
 
 import jakarta.annotation.Nonnull;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
 
+@Slf4j
 class NiceUnicornService implements UnicornService {
 
     @Nonnull
     @Override
     public List<String> readUnicorns() {
+        log.info("readUnicorns() on NiceUnicornService happily started");
+
         return List.of(
             "Sternenlicht Glanzfeder",
             "Glitzerglanz Sternenzauber",
--- fakeservice-demo-fake/src/main/java/com/raketeneinhorn/spring/boot/fakeservice/demo/fake/core/unicorn/EvilUnicornService.java
+++ fakeservice-demo-fake/src/main/java/com/raketeneinhorn/spring/boot/fakeservice/demo/fake/core/unicorn/EvilUnicornService.java
@@ -2,11 +2,13 @@ package com.raketeneinhorn.spring.boot.fakeservice.demo.fake.core.unicorn;
 
 import com.raketeneinhorn.spring.boot.fakeservice.demo.core.unicorn.UnicornService;
 import jakarta.annotation.Nonnull;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Primary
 @Service
 public class EvilUnicornService implements UnicornService {
@@ -14,6 +16,8 @@ public class EvilUnicornService implements UnicornService {
     @Nonnull
     @Override
     public List<String> readUnicorns() {
+        log.info("readUnicorns() on EvilUnicornService fearfully started");
+
         return List.of(
             "Dunkelhorn Schattenklaue",
             "Nachtschatten Finsterglut",
